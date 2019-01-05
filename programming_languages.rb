260beaa5ac28afb6836cf0b0fa57e73502d486f5
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-05 19:30:21 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,6 @@ new_languages = {}
 languages.each do |style, language|
   language.each do |name, attributes|
   new_languages[name] ||= attributes
-  new_languages[name][:style]={}
   new_languages[name][:style]=[]
   new_languages[name][:style]<<style
 end
